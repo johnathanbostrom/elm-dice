b42c1321b5c6422562c9334a3b6124c2d992b475
Johnathan <orrosta@gmail.com>
2020-02-25 20:44:23 -0600

    format

--- src/Dice.elm
+++ src/Dice.elm
@@ -1,40 +1,48 @@
-module Dice exposing (Dice(..), roll, Rules(..), andThen, plus, RollResult, RollResults(..), justResults, constant, Keep(..))
-import Random
-import Maybe
-import List.Extra exposing (maximumBy, minimumBy)
+module Dice exposing (Dice(..), Keep(..), RollResult, RollResults(..), Rules(..), andThen, constant, justResults, plus, roll)
 
-type Dice = D4
+import List.Extra exposing (maximumBy, minimumBy)
+import Maybe
+import Random
+
+
+type Dice
+    = D4
     | D6
     | D8
     | D10
     | D12
     | D20
     | D100
-    | DX Int 
+    | DX Int
     | DicePool Int Dice
     | Custom String (Random.Generator RollResult)
 
 
-type Rules = DropLowest
+type Rules
+    = DropLowest
     | CountSuccessesIf (Int -> Bool)
     | ExplodeIf (Int -> Bool)
     | RerollIf (Int -> Bool) Keep
-    --|EndlessRerollIf (Int -> Bool) Keep 
+      --|EndlessRerollIf (Int -> Bool) Keep
     | Do (RollResult -> Random.Generator RollResult)
 
-type RollResults = Empty
+
+type RollResults
+    = Empty
     | RollResults (List RollResult)
 
 
 type alias RollResult =
-        { description: String
-        , value: Int
-        , children: RollResults
-        }
+    { description : String
+    , value : Int
+    , children : RollResults
+    }
+
 
 oneDieResult : String -> Int -> RollResult
 oneDieResult desc val =
     RollResult desc val Empty
+
 
 manyDieResult : String -> Int -> List RollResult -> RollResult
 manyDieResult desc val rolls =
@@ -44,33 +52,36 @@ manyDieResult desc val rolls =
 constant : Int -> Random.Generator RollResult
 constant val =
     Random.constant val
-    |> Random.map (oneDieResult "Constant")
+        |> Random.map (oneDieResult "Constant")
+
 
 plus : Random.Generator RollResult -> Random.Generator RollResult -> Random.Generator RollResult
 plus diceA diceB =
-    Random.map2 (\a b -> combineResults (a.description ++ " + " ++ b.description) [a,b] ) diceA diceB
+    Random.map2 (\a b -> combineResults (a.description ++ " + " ++ b.description) [ a, b ]) diceA diceB
 
 
 dX : Int -> Random.Generator RollResult
-dX sides = 
+dX sides =
     Random.int 1 sides
-    |> Random.map (oneDieResult ("D" ++ String.fromInt sides))
+        |> Random.map (oneDieResult ("D" ++ String.fromInt sides))
+
 
 roll : Int -> Dice -> Random.Generator RollResult
 roll numDice dieType =
     if numDice > 1 then
         toGenerator <| DicePool numDice dieType
+
     else if numDice == 0 then
         constant 0
-    else 
+
+    else
         toGenerator dieType
 
 
 toDie : String -> Random.Generator Int -> Dice
 toDie description generator =
     Random.map (oneDieResult description) generator
-    |>  Custom description
-
+        |> Custom description
 
 
 toGenerator : Dice -> Random.Generator RollResult
@@ -78,119 +89,150 @@ toGenerator dieType =
     case dieType of
         D4 ->
             dX 4
+
         D6 ->
             dX 6
+
         D8 ->
             dX 8
+
         D10 ->
             dX 10
+
         D12 ->
             dX 12
+
         D20 ->
             dX 20
+
         D100 ->
             dX 100
-        DX sides->
-            dX sides 
+
+        DX sides ->
+            dX sides
+
         DicePool numDice dieTypes ->
             dicePool numDice dieTypes
+
         Custom description generator ->
             dCustom description generator
 
+
 dCustom : String -> Random.Generator RollResult -> Random.Generator RollResult
 dCustom desc generator =
-    Random.map (\r -> {r | description = desc}) generator
+    Random.map (\r -> { r | description = desc }) generator
 
 
 dConstant : Int -> Random.Generator RollResult
 dConstant val =
     oneDieResult "constant" val
-    |> Random.constant
+        |> Random.constant
+
 
 dicePool : Int -> Dice -> Random.Generator RollResult
 dicePool numDice dieTypes =
     toGenerator dieTypes
-    |> Random.list numDice
-    |> Random.map (combineResults (String.fromInt numDice ++ " " ++ dieName dieTypes))
+        |> Random.list numDice
+        |> Random.map (combineResults (String.fromInt numDice ++ " " ++ dieName dieTypes))
 
- 
+
 combineResults : String -> List RollResult -> RollResult
-combineResults description rollResults = 
+combineResults description rollResults =
     let
-        val = List.map .value rollResults |> List.foldl (+) 0
+        val =
+            List.map .value rollResults |> List.foldl (+) 0
     in
-        manyDieResult description val rollResults
-
+    manyDieResult description val rollResults
 
 
 andThen : Rules -> Random.Generator RollResult -> Random.Generator RollResult
 andThen rule rolls =
     case rule of
-        DropLowest -> 
+        DropLowest ->
             Random.map dropLowest rolls
+
         CountSuccessesIf test ->
             Random.map (countSuccessesIf test) rolls
-        ExplodeIf test ->   
-            explodeIf test rolls   
+
+        ExplodeIf test ->
+            explodeIf test rolls
+
         RerollIf test keep ->
             rerollIf test keep rolls
+
         Do action ->
             Random.andThen action rolls
+
 
 dieName : Dice -> String
 dieName dieType =
     case dieType of
         D4 ->
             "D4"
+
         D6 ->
             "D6"
+
         D8 ->
             "D8"
+
         D10 ->
             "D10"
+
         D12 ->
             "D12"
+
         D20 ->
             "D20"
+
         D100 ->
             "D100"
-        DX sides->
+
+        DX sides ->
             "D" ++ String.fromInt sides
+
         DicePool numDice dT ->
             String.fromInt numDice ++ "D" ++ dieName dT
+
         Custom description _ ->
             description
-
 
 
 justResults : List RollResult -> List Int
 justResults rolls =
     List.map .value rolls
 
---andThen helpers
 
+
+--andThen helpers
 --TODO: reroll keep high, low.  How many times?
-type Keep = Low
+
+
+type Keep
+    = Low
     | High
     | New
     | KeepCustom (List RollResult -> RollResult)
 
+
 rerollIf : (Int -> Bool) -> Keep -> Random.Generator RollResult -> Random.Generator RollResult
 rerollIf test keep generator =
     generator
-    |> Random.andThen (reroll generator test)
-    |> Random.map (combineResults "Reroll")
-    |> Random.map (chooseReroll keep)
+        |> Random.andThen (reroll generator test)
+        |> Random.map (combineResults "Reroll")
+        |> Random.map (chooseReroll keep)
+
 
 reroll : Random.Generator RollResult -> (Int -> Bool) -> RollResult -> Random.Generator (List RollResult)
 reroll generator test rollResult =
     if test rollResult.value then
         Random.constant rollResult
-        |> Random.list 1                
-        |> Random.map2 (::) generator
+            |> Random.list 1
+            |> Random.map2 (::) generator
+
     else
         Random.constant rollResult
-        |> Random.list 1    
+            |> Random.list 1
 
 
 chooseReroll : Keep -> RollResult -> RollResult
@@ -198,62 +240,67 @@ chooseReroll keep rollResult =
     case rollResult.children of
         Empty ->
             rollResult
+
         RollResults rolls ->
             let
                 keptRoll =
                     case keep of
                         High ->
                             List.map .value rolls
-                            |> List.maximum
-                            |> Maybe.withDefault 0
+                                |> List.maximum
+                                |> Maybe.withDefault 0
+
                         Low ->
                             List.map .value rolls
-                            |> List.minimum
-                            |> Maybe.withDefault 0
-                        New -> 
+                                |> List.minimum
+                                |> Maybe.withDefault 0
+
+                        New ->
                             List.map .value rolls
-                            |> List.head
-                            |> Maybe.withDefault 0
+                                |> List.head
+                                |> Maybe.withDefault 0
+
                         KeepCustom action ->
                             (action rolls).value
             in
-                {rollResult | value = keptRoll}
-            
-
-
+            { rollResult | value = keptRoll }
 
 
 explodeIf : (Int -> Bool) -> Random.Generator RollResult -> Random.Generator RollResult
 explodeIf test generator =
     generator
-    |> Random.andThen (explode generator test)
-    |> Random.map (combineResults "Explode!")
+        |> Random.andThen (explode generator test)
+        |> Random.map (combineResults "Explode!")
 
 
 explode : Random.Generator RollResult -> (Int -> Bool) -> RollResult -> Random.Generator (List RollResult)
 explode generator test rollResult =
     if test rollResult.value then
         Random.constant rollResult
-        |> Random.list 1                
-        |> Random.map2 (++) (generator |> Random.andThen (explode generator test))
-    else 
+            |> Random.list 1
+            |> Random.map2 (++) (generator |> Random.andThen (explode generator test))
+
+    else
         Random.constant rollResult
-        |> Random.list 1
+            |> Random.list 1
 
 
 dropLowest : RollResult -> RollResult
 dropLowest result =
     case result.children of
         Empty ->
-            RollResult "No Dice" 0 Empty
+            { result | value = 0 }
+
         RollResults rolls ->
             let
-                newValue = List.map .value rolls
-                    |> List.minimum
-                    |> Maybe.withDefault 0
-                    |> (-) result.value
+                newValue =
+                    List.map .value rolls
+                        |> List.minimum
+                        |> Maybe.withDefault 0
+                        |> (-) result.value
             in
-                {result | value = newValue }
+            { result | value = newValue }
+
 
 countSuccessesIf : (Int -> Bool) -> RollResult -> RollResult
 countSuccessesIf test rollResult =
@@ -262,16 +309,18 @@ countSuccessesIf test rollResult =
             case rollResult.children of
                 Empty ->
                     succeedVal test rollResult
+
                 RollResults rolls ->
                     List.map (succeedVal test) rolls
-                    |> List.sum
+                        |> List.sum
     in
-        {rollResult | value = successes}
-    
+    { rollResult | value = successes }
+
 
 succeedVal : (Int -> Bool) -> RollResult -> Int
 succeedVal test rollResult =
     if test rollResult.value then
         1
-    else 
+
+    else
         0
--- src/SimpleRoller.elm
+++ src/SimpleRoller.elm
@@ -1,10 +1,13 @@
 module SimpleRoller exposing (main)
+
 import Browser
 import Dice exposing (..)
-import Random exposing(generate)
-import Html exposing (Html, div, button, text, span)
+import Html exposing (Html, button, div, span, text)
 import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
+import Random exposing (generate)
+
+
 
 -- APP
 
@@ -21,12 +24,12 @@ main =
 
 init : ( Model, Cmd Msg )
 init =
-    ( { rolls = [ ]}
+    ( { rolls = [] }
     , Cmd.none
     )
- 
 
- 
+
+
 -- MODEL
 
 
@@ -48,11 +51,12 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         RollDice num dice ->
-            ( model 
+            ( model
             , rollDicePoolNTimes num dice
             )
+
         RollResult newRolls ->
-            ( {model | rolls = newRolls }
+            ( { model | rolls = newRolls }
             , Cmd.none
             )
 
@@ -60,89 +64,101 @@ update msg model =
 rollDicePoolNTimes : Int -> Dice -> Cmd Msg
 rollDicePoolNTimes num dieType =
     roll 1 dieType
-    |> Random.list num
-    |> Random.generate RollResult
+        |> Random.list num
+        |> Random.generate RollResult
+
 
 statGen : Dice
 statGen =
     roll 4 D6
-    |> andThen DropLowest
-    |> Custom "4D6 Drop Lowest"
+        |> andThen DropLowest
+        |> Custom "4D6 Drop Lowest"
+
 
 plusRoll : Dice
 plusRoll =
     roll 3 D6
-    |> andThen DropLowest
-    |> plus (roll 2 D4)
-    |> Custom "3D6 Drop Lowest plus 2D4"
+        |> andThen DropLowest
+        |> plus (roll 2 D4)
+        |> Custom "3D6 Drop Lowest plus 2D4"
+
 
 succeedOnEightRoll : Dice
 succeedOnEightRoll =
-    roll 6 explodingD10
-    |> Custom "exploding Dice"
+    roll 10 explodingD10
+        |> Custom "Successes"
 
-explodingD10 : Dice 
-explodingD10 = 
-    roll 1 D10  
-    |> andThen (ExplodeIf (\x -> x > 9))
-    |> andThen (CountSuccessesIf (\x -> x > 7))
-    |> Custom "exploding Dice"
+
+explodingD10 : Dice
+explodingD10 =
+    roll 1 D10
+        |> andThen (ExplodeIf (\x -> x > 9))
+        |> andThen (CountSuccessesIf (\x -> x > 7))
+        |> Custom "exploding Dice"
+
 
 aT : Dice
 aT =
-    roll 3 D6   
-    |> andThen (Do (\x -> Random.constant {x | value = -100}))
-    |> Custom "test"
+    roll 3 D6
+        |> andThen (Do (\x -> Random.constant { x | value = -100 }))
+        |> Custom "test"
 
 
 rerollIf : Dice
 rerollIf =
     roll 1 statGen
-    |> andThen (RerollIf (\x -> x < 10) High)
-    |> Custom "reroll < 3"
+        |> andThen (RerollIf (\x -> x < 16) High)
+        |> Custom "reroll < 3"
 
-rResultToString : (List RollResult) -> String
+
+rResultToString : List RollResult -> String
 rResultToString results =
-    List.map (\r ->  rollToString r) results
-    |> String.join "~"
+    List.map (\r -> rollToString r) results
+        |> String.join "~"
+
 
 rollToString : RollResult -> String
 rollToString result =
     case result.children of
         Empty ->
-            result.description ++ ":  " ++ (String.fromInt result.value)
+            result.description ++ ":  " ++ String.fromInt result.value
+
         RollResults rolls ->
-            (String.fromInt result.value) ++ ":  Rolls:  " ++ (rResultToString rolls)
+            String.fromInt result.value ++ ":  Rolls:  " ++ rResultToString rolls
+
 
 dResultToString : List RollResult -> String
-dResultToString result = 
+dResultToString result =
     List.map (\r -> r.description ++ ": " ++ String.fromInt r.value) result
-    |> String.join " "
+        |> String.join " "
+
+
 
 -- VIEW
 
 
 view : Model -> Html Msg
 view model =
-    div [] [renderDice model]
+    div [] [ renderDice model ]
 
 
-renderDice : Model-> Html Msg
+renderDice : Model -> Html Msg
 renderDice model =
     div
-        [ ]
+        []
         [ text <| String.join "  " <| List.map String.fromInt <| justResults model.rolls
         , diceButtons
         , renderExpandedResults model.rolls
         ]
 
+
 diceButtons : Html Msg
-diceButtons = 
-    div [] 
-        [ diceButton D4 "D4" 
+diceButtons =
+    div []
+        [ diceButton D4 "D4"
         , diceButton D6 "D6"
         , diceButton D8 "D8"
-        , diceButton D10 "D10" 
+        , diceButton D10 "D10"
         , diceButton D12 "D12"
         , diceButton D20 "D20"
         , diceButton D100 "D100"
@@ -154,40 +170,49 @@ diceButtons =
         , diceButton rerollIf "reroll"
         ]
 
+
 diceButton : Dice -> String -> Html Msg
 diceButton dieType dieName =
-    button [ onClick <| RollDice 1 dieType ] [text dieName]
+    button [ onClick <| RollDice 6 dieType ] [ text dieName ]
 
 
 renderExpandedResults : List RollResult -> Html Msg
 renderExpandedResults rolls =
     div [] (List.map renderRoll rolls)
 
+
 renderExpandedResult : RollResult -> Html Msg
 renderExpandedResult roll =
     let
-        valueString = roll.description ++ ":  " ++ String.fromInt roll.value
+        valueString =
+            roll.description ++ ":  " ++ String.fromInt roll.value
     in
-        case roll.children of
-            Empty ->
-                span [style "margin-right" ".5em"] [text <| valueString]
-            RollResults rolls ->
-                div [style "margin-left" "2em"] ([text <| valueString ++ "  Rolls:  "] ++ (List.map renderExpandedResult rolls))
+    case roll.children of
+        Empty ->
+            span [ style "margin-right" ".5em" ] [ text <| valueString ]
+
+        RollResults rolls ->
+            div [ style "margin-left" "2em" ] ([ text <| valueString ++ "  Rolls:  " ] ++ List.map renderExpandedResult rolls)
 
 
 renderRoll : RollResult -> Html Msg
 renderRoll roll =
     let
-        valueString = roll.description ++ ":  " ++ String.fromInt roll.value
+        valueString =
+            roll.description ++ ":  " ++ String.fromInt roll.value
     in
-        case roll.children of
-            Empty ->
-                span [style "margin-right" ".5em"] [text <| valueString]
-            RollResults rolls ->
-                let
-                    children = div [style "margin-left" "2em"] (List.map renderRoll rolls)
-                in
-                    div [] [text <| valueString, children]
+    case roll.children of
+        Empty ->
+            span [ style "margin-right" ".5em" ] [ text <| valueString ]
+
+        RollResults rolls ->
+            let
+                children =
+                    div [ style "margin-left" "2em" ] (List.map renderRoll rolls)
+            in
+            div [] [ text <| valueString, children ]
+
+
 
 -- SUBSCRIPTIONS
 
